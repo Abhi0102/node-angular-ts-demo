647189dd63ef196f68fa58f70f239d23aa310e3e
Abhishek Sharma <67204033+Abhi0102@users.noreply.github.com>
2023-05-24 20:05:49 +0530

    changing sonar prop to previous ones

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,7 +5,6 @@ import { config } from '../config/azureConfig';
 const router = Router();
 
 router.route('/v1').get((req, res) => {
-  //   console.log(req);
   logger.info(config);
   logger.error('On route v1');
   res.status(200).json({ success: true, message: 'Successfully accessed' });
