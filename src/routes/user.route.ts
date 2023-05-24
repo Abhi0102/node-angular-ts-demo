1b0fb6deb6414226d611b0eb3603b5f6a2ddf579
Abhishek Sharma <67204033+Abhi0102@users.noreply.github.com>
2023-05-24 20:26:42 +0530

    fixing url error

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,18 @@
 import logger from '../config/logger';
-import { Router } from 'express';
+import { Router, RequestHandler } from 'express';
 import { config } from '../config/azureConfig';
 import axios from 'axios';
 
 const router = Router();
 
-router.route('/v1/:id').get(async (req, res) => {
+router.route('/v1/:id').get((async (req, res, next) => {
   logger.info(config);
   logger.error('On route v1');
+  if (!req.params.id) {
+    return next(new Error());
+  }
   await axios.get(`https://fakestoreapi.com/products/${req.params.id}`);
   res.status(200).json({ success: true, message: 'Successfully accessed' });
-});
+}) as RequestHandler);
 
 export default router;
