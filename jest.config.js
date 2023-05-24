3ed21ac134e0a97cade2a744a2b636487077b755
Abhishek Sharma <67204033+Abhi0102@users.noreply.github.com>
2023-05-24 19:57:17 +0530

    adding props

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  testResultsProcessor: 'jest-sonar-reporter',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/*.spec.ts'],
   coverageDirectory: './coverage',
