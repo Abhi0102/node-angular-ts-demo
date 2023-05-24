58e9f529ffab66cbc90a4deb0819da9aadde8c6a
Abhishek Sharma <67204033+Abhi0102@users.noreply.github.com>
2023-05-24 20:20:13 +0530

    making api call- replicating url error

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,14 @@
 import logger from '../config/logger';
 import { Router } from 'express';
 import { config } from '../config/azureConfig';
+import axios from 'axios';
 
 const router = Router();
 
-router.route('/v1').get((req, res) => {
+router.route('/v1/:id').get(async (req, res) => {
   logger.info(config);
   logger.error('On route v1');
+  await axios.get(`https://fakestoreapi.com/products/${req.params.id}`);
   res.status(200).json({ success: true, message: 'Successfully accessed' });
 });
 
